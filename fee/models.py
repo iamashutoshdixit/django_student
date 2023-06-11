3fd205cf44d57e211c1080088f4b421438730e71
Ashutosh Dixit <ashutoshdixit@Ashutoshs-MacBook-Air.local>
2023-06-11 17:50:07 +0530

    django demo project

---
+++ fee/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Students(models.Model):  
+    first_name = models.CharField(max_length=200)  
+    last_name = models.CharField(max_length=200)  
+    address = models.CharField(max_length=200)  
+    roll_number = models.IntegerField()  
+    mobile = models.CharField(max_length=10)  
---
+++ fee/urls.py
@@ -0,0 +1,4 @@
+from .views import StudentView  
+from django.urls import path  
+  
+ 
